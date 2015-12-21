c1306cbeb55b2751b5a17f1ab2d68cb702618d0d
Sripadmanaban Subramanian <sripadmanabans@gmail.com>
2015-12-20 21:56:52 -0600

    Main Activity modified.

--- app/src/main/java/com/sripad/staggeredgrid/StaggeredActivity.java
+++ app/src/main/java/com/sripad/staggeredgrid/StaggeredActivity.java
@@ -1,15 +1,13 @@
 package com.sripad.staggeredgrid;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class StaggeredActivity extends AppCompatActivity {
+import com.sripad.staggeredgrid.base.BaseActivity;
+
+public class StaggeredActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +15,6 @@ public class StaggeredActivity extends AppCompatActivity {
         setContentView(R.layout.activity_staggered);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
